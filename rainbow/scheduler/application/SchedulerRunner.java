809087a0132c4e6679aefc2a44d26b15ed39a5fe
Wesley <lukwesley98@hotmail.com>
2012-03-25 22:14:53 -0700

    Removed the last string switch, hopefully should be all  the java 7 stuff

--- rainbow/scheduler/application/SchedulerRunner.java
+++ rainbow/scheduler/application/SchedulerRunner.java
@@ -21,37 +21,33 @@ public class SchedulerRunner {
 			do {
 				String line = s.nextLine();
 				String[] params = line.split(" ");
-				switch (params[0]) {
-					case "quit":
-						done = true;
-						break;
-					case "query":
-						ss.newQuery(params[1]);
-						break;
-					case "test1":
-						// md5 of "z"
-						ss.newQuery("fbade9e36a3f36d3d676c1b808451dd7");
-						break;
-					case "test2":
-						// md5 of "test"
-						ss.newQuery("098f6bcd4621d373cade4e832627b4f6");
-						break;
-					case "test3":
-						// md5 of "asdfgh"
-						ss.newQuery("a152e841783914146e4bcd4f39100686");
-						break;
-					case "test4":
-						// md5 of "asdfgh"
-						ss.newQuery("c83b2d5bb1fb4d93d9d064593ed6eea2");
-						break;
-					default:
-						System.out.println("Unknown command " + params[0]);
-						break;
+				String command = params[0];
+				// Too lazy to make a mapping just live with an if else chain for now
+				if (command.equals("quit")) {
+					done = true;
+				} else if (command.equals("query")) {
+					ss.newQuery(params[1]);
+				} else if (command.equals("test1")) {
+					// md5 of "z"
+					ss.newQuery("fbade9e36a3f36d3d676c1b808451dd7");
+				} else if (command.equals("test2")) {
+					// md5 of "test"
+					ss.newQuery("098f6bcd4621d373cade4e832627b4f6");
+				} else if (command.equals("test3")) {
+					// md5 of "asdfgh"
+					ss.newQuery("a152e841783914146e4bcd4f39100686");
+				} else if (command.equals("test4")) {
+					// md5 of "asdfgh"
+					ss.newQuery("c83b2d5bb1fb4d93d9d064593ed6eea2");
+				} else {
+
+					System.out.println("Unknown command " + params[0]);
 				}
 			} while (!done);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 		ss.callInterrupt();
 	}
 }
